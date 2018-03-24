db50582a3f37fb6f14eab570aa19af8d9a0b1f48
Srija S <srijakumar7@gmail.com>
2018-03-24 12:58:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const logDriversByHometown = function(drivers, location){
 
 const driversByRevenue = function(drivers){
   drivers.sort(function(driver1, driver2){
-    return driver1 - driver2;
+    return driver1.revenue - driver2.revenue;
   }
-    
+
   );
 }
