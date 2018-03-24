e0cf6c4d3a0fe96165706d5422759ebeefcde457
Srija S <srijakumar7@gmail.com>
2018-03-24 12:50:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+
+const logDriverNames = function(drivers){
+    drivers.forEach(function (driver){
+    console.log(driver.name)
+});
+}
+
+const logDriversByHometown = function(drivers, location){
+  drivers.forEach(function(driver){
+    if (driver.hometown == location) {
+      console.log(driver.name);
+    }
+  }
+);
+}
+
+const driversByRevenue = function(drivers){
+  sort.drivers.localeCompare(driver)
+}
